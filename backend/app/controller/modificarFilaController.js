d4b655c4c9197f972446e8bf7e4d109d9bcf658e
Gonzalo Sempol <gonzalo.sempol@gmail.com>
2022-08-30 21:05:49 -0300

    Se agrega modificarFila

--- backend/app/controller/modificarFilaController.js
+++ backend/app/controller/modificarFilaController.js
@@ -1,5 +1,36 @@
 const pgClient = require("../db/postgres")
 async function modificarFilaController (req,res) {
-    return res.send("Hola"); 
+
+    //UPDATE table_name
+    //SET column_name1= value1, column_name2= value2
+    //WHERE condition;
+
+
+
+    const {body: {tableName, tableIDs, tableStructure}} = req; 
+    let sqlString = 'UPDATE public."' + tableName +'" SET '
+    let sqlUpdateCondition = ' WHERE '
+
+    let i = 0;
+    
+    //recorro el array de las columnas
+    while(i < tableStructure.length){
+        
+        sqlString += tableStructure[i].columnName + "=" + tableStructure[i].newValue + ((i != tableStructure.length-1) ? ", " : ""); //el ultimo no lleva coma
+        i++;
+    }
+    i=0;
+    //recorro el array de los IDs
+    while(i < tableIDs.length){
+        
+        sqlUpdateCondition += tableIDs[i].columnName + "=" + tableIDs[i].value + ((i != tableIDs.length-1) ? " AND " : ""); //el ultimo (o si es uno solo) no lleva AND
+        i++;
+    }
+
+    sqlString += sqlUpdateCondition
+    const response = await pgClient.query(sqlString);
+    return res.send(response); 
+    
+
 }
 module.exports = modificarFilaController
