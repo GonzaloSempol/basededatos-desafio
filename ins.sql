3fe43d3e795c3155e1bf43b10d60413a5121a6e4
Gonzalo Sempol <gonzalo.sempol@gmail.com>
2022-09-02 12:10:14 -0300

    sql para test

---
+++ ins.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Public."TablaNueva" (nombre, descripcion) VALUES('1d', 'desc1');
+INSERT INTO Public."TablaNueva" (nombre, descripcion) VALUES('pepe2', 'desc2');
+INSERT INTO Public."TablaNueva" (nombre, descripcion) VALUES('pepe3', 'desc3');
+SELECT * FROM Public."TablaNueva"
+
